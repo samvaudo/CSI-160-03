0219140ecbc63b8ea4867b455454874e2660257f
samuel.vaudo <samuel.vaudo@mymail.champlain.edu>
2023-09-04 14:14:21 -0400

    coding class

--- csiproject/introduction.py
+++ csiproject/introduction.py
@@ -8,3 +8,32 @@ It supports multiple lines of comments
 """
 Same comment with double quotes
 """
+
+'''
+Rules for variable creation
+1. Dont start with a number
+    1a
+        cant be a variable
+
+2. No spaces
+    my variable
+        cant be a variable
+3. No keywords
+    print1
+        cant be a variable
+
+4. No special characters
+    MyVarialbe!
+        cant be a variable
+'''
+
+#a variable
+x1 = 1
+x2 = 4
+Why_not = "why not"
+FirstName = "Name"
+#print = "hello world"
+
+result = x2 - x1
+print(result)
+
