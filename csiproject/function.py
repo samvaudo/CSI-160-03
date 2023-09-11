8cd52174ad03c12601a18f43c43d6b5ff597623a
samuel.vaudo <samuel.vaudo@mymail.champlain.edu>
2023-09-11 13:09:45 -0400

    Create function.py

---
+++ csiproject/function.py
@@ -0,0 +1,14 @@
+#how to work with functions
+
+
+x = 0
+y = ""
+def CountNumbers():
+    pass
+
+CountNumbers() #calls function
+
+def vairable_management():
+    print(x)
+    z = 1 + 2
+    print(z)
