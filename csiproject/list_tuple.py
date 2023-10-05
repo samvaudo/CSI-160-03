2148669102504f21664e2f51ca85b7f5e3f97cd9
samuel.vaudo <samuel.vaudo@mymail.champlain.edu>
2023-10-05 13:27:09 -0400

    Create list_tuple.py

---
+++ csiproject/list_tuple.py
@@ -0,0 +1,25 @@
+# Working with lists and tuples
+
+x = [1, 2, 4, 6, 8, 0]
+y = x
+
+# print("The original 'x' list:",x)
+x.append(9)
+x.append(3)
+x.insert(0, 0)
+
+# print("The new 'x' list:",x)
+
+# print("Original 'y' list:",y)
+
+
+a = [1, 2, 4, 6, 8, 0]
+b = tuple(a)  # Converts a list to a tuple data structure
+print(a)
+print(b)
+
+c = b
+print(c)
+
+# b.append(4)
+print(b)
