1cb4d2e79240c0fdbad64be94ce2f0bcf7e099b3
samuel.vaudo <samuel.vaudo@mymail.champlain.edu>
2023-10-23 13:25:14 -0400

    lab reorg

---
+++ csiproject/f_strings.py
@@ -0,0 +1,24 @@
+# Working with f_strings (format strings)
+
+name = "Jane"
+age = 25
+
+
+def format_intro():
+    print("Hello, {}! You're {} years old.".format(name, age))
+    print("Hello,", name + "! You're", age, "years old.")
+
+
+# format_intro()
+
+
+def dict_intro():
+    my_dictionary = {'name': 'John', 'name': 'Jane'}  # {key:value}
+    print(my_dictionary)
+
+
+# dict_intro()
+
+# Modulo Operator, %
+
+
