159e33c7c2359f2ec94e312529987c6057cd4d38
samuel.vaudo <samuel.vaudo@mymail.champlain.edu>
2023-10-30 13:03:51 -0400

    dictionaries

---
+++ csiproject/lessons/dictionaries.py
@@ -0,0 +1,5 @@
+# An intro to dictionaries
+
+example = {'key': 'value'}
+
+courses = {'CSI-160': 'Intro to Python'}
--- csiproject/lessons/format_strings/review_funct.py
+++ csiproject/lessons/format_strings/review_funct.py
@@ -1,15 +1,17 @@
-#Reviewing functions, param, placeholders & saving data
+# Reviewing functions, param, placeholders & saving data
 
 def acct_info():
     name = input("User name:")
     pswd = input("Password:")
     acct = input("Accout number:")
     print("thanks for the data!")
-    storeData(name,pswd,acct)
+    storeData(name, pswd, acct)
 
-def storeData(name,pswd,acct):
-    print("Username is",name)
-    print("Password is",pswd)
-    print("Account number is",acct)
+
+def storeData(name, pswd, acct):
+    print("Username is", name)
+    print("Password is", pswd)
+    print("Account number is", acct)
+
 
 acct_info()
