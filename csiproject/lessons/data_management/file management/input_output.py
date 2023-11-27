f891ad319f2e6f784e01ee8658d536c2d4faac43
samuel.vaudo <samuel.vaudo@mymail.champlain.edu>
2023-11-27 14:10:36 -0500

    class

--- csiproject/lessons/data_management/file management/input_output.py
+++ csiproject/lessons/data_management/file management/input_output.py
@@ -14,7 +14,33 @@ File handler: Manages the use of open(), close(), write(), read(), and append()
 IF YOU HAVE AN open(), THERE MUST BE A close()
 '''
 
-f = open('first_file.txt', 'w')
-content = 'This is text data'
-f.write(content)
-f.close
+
+'''
+#Writing to a file
+f1 = open('first_file.txt', 'a') #Opens in append mode
+content1 = 'More data. ' #content
+f1.write(content1) # adds to data
+f1.close() #closes file
+
+#Reading data from a file
+f2 = open('first_file.txt', 'r') #Opens in read mode
+content2 = f2.read() #Reads file
+print(content2) #Prints content
+f2.close() #Closes file
+
+#Context Manager - It automatically closes any open files in a process
+with open('first_file.txt','r') as f3:
+    content3 = f3.read()
+    print('You are reading from the file "first_file.txt"')
+    print(content3)
+'''
+
+file = 'first_file.txt'
+data = input('Enter data for this file:')
+
+with open(file,'a') as f:
+    f.write(data)
+
+with open(file,'r') as f2:
+    content = f2.read(5) #This can control the number of characters to output
+    print(content)
